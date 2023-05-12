cc8142bac0561ec488a0c3821cc4e09b5a5badb1
pradeepsurya <pradeepsurya@live.com>
2023-05-11 23:58:23 -0400

    application starter script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Navigate to the server directory, install dependencies and run the Flask app
+cd server
+pip install -r requirements.txt
+export FLASK_APP=app.py
+export FLASK_ENV=development
+flask run --port=5000 &
+echo "Flask server is running on http://localhost:5000"
+
+# Wait for 3 seconds to allow Flask to start
+sleep 3
+
+# Navigate to the client directory, install dependencies and run the Angular app
+cd ../client
+npm install
+ng serve &
+echo "Angular app is running on http://localhost:4200"
+
+# Wait for both processes to finish
+wait
